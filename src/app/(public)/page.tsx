4aedab5ee0abfa60d74347646b8e2fa3ced6f2b6
zakiabashir <nshafeys0@gmail.com>
2024-10-01 14:22:44 +0500

    push my code

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,17 +1,18 @@
 
+
 export default function Home() {
   return (
     
      <div>
       
-      <h1 className="textbolder-900  text-red-900 ">Wellcome to my web Page </h1><br />
+      <h1 className='textbolder-900  text-red-900 '>Wellcome to my web Page </h1><br />
       <h3>Governor House Sindh (GIAIC)</h3><br />
-          <p className="text-center  ">
+          <p className='text-center  '>
             I am currently enrolled in the  "Cloud Computing and Generative
-            Artificial Intelligence"  program at Governor House Sindh, refining
-            my skills in advanced technologies.  This course empowers me to
-            integrate cutting-edge cloud solutions  and AI-driven innovations
-            into development projects, optimizing workflows  and enhancing user
+            Artificial Intelligence" <br /> program at Governor House Sindh, refining
+            my skills in advanced technologies. <br /> This course empowers me to
+            integrate cutting-edge cloud solutions <br /> and AI-driven innovations
+            into development projects, optimizing workflows <br /> and enhancing user
             experiences.
           </p>
      </div>
