a8507d6fd28bf170c6632762df0fb992ac093852
zakiabashir <nshafeys0@gmail.com>
2024-10-02 01:53:06 +0500

    push my code

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 //import localFont from "next/font/local";
-//import "./globals.css";
+import "../globals.css";
 
 import Header from "../components/header"
 import Footer from "../components/footer"
